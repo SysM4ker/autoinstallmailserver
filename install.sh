3ca59e1aa1d4ac175caa7f2f8479ef92261cee79
SysM4ker <SysM4ker@users.noreply.github.com>
2024-11-23 08:46:09 +0100

    New

--- install.sh
+++ install.sh
@@ -1,1 +1,33 @@
 #!/bin/bash
+
+#Complete machine update
+apt -y update && apt -y upgrade
+
+#install all the important things for a mail server
+apt install cron nginx snapd net-tools -y
+
+#https://docs.docker.com/engine/install/debian/#install-using-the-repository
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+# To install the latest version, run:
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+mkdir /home/mail
+
+#Install poste.io with docker
+docker run --net=host -e TZ=Europe/Paris -v /home/mail:/data --name "mailserver" -h "mail.example.com" -e "HTTP_PORT=8080" -e "HTTPS_PORT=4433" -e "DISABLE_CLAMAV=TRUE" -t analogic/poste.io
+
+#-e "DISABLE_CLAMAV=TRUE" To disable ClamAV, it is useful for low mem usage.
+#-e "DISABLE_RSPAMD=TRUE" To disable Rspamd, it is useful for low mem usage.
+
