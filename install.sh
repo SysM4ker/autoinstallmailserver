690459411ab2b9a17cd0e4113a75c16810b12194
SysM4ker <SysM4ker@users.noreply.github.com>
2024-12-05 21:44:13 +0100

    poste.io install

--- install.sh
+++ install.sh
@@ -33,23 +33,26 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 mkdir /home/mail
+CONTAINER_NAME="mailserver"
 
 #
 # echo "Your domain name example mail.domainname.tld"
 # read domain
-read -p "Enter your domain (ex: mail.exemple.tld) : " domain
+read -p "Enter your domain (ex: mail.exemple.tld) : " DOMAIN
 
 #Install poste.io with docker
-sudo docker run -d --net=host -e TZ=Europe/Paris -v /home/mail:/data --name "mailserver" -h "$domain" -e "HTTP_PORT=8080" -e "HTTPS_PORT=4433" -e "DISABLE_CLAMAV=TRUE" -t analogic/poste.io
+sudo docker run -d --net=host -e TZ=Europe/Paris -v /home/mail:/data --name "mailserver" -h "$DOMAIN" -e "HTTP_PORT=8080" -e "HTTPS_PORT=4433" -e "DISABLE_CLAMAV=TRUE" -t analogic/poste.io
 
 #-e "DISABLE_CLAMAV=TRUE" To disable ClamAV, it is useful for low mem usage.
 #-e "DISABLE_RSPAMD=TRUE" To disable Rspamd, it is useful for low mem usage.
 
+#Crontabe configuration so that the docker can be launched on startup
+(crontab -l 2>/dev/null; echo "@reboot docker start $CONTAINER_NAME") | crontab -
 
 ##Configuration nginx
 
 sudo wget -O /etc/nginx/sites-available/mail https://raw.githubusercontent.com/SysM4ker/autoinstallmailserver/refs/heads/main/confpostenginx
-sudo sed -i "s/mail\.domaine\.tld/$domain/g" /etc/nginx/sites-available/mail
+sudo sed -i "s/mail\.domaine\.tld/$DOMAIN/g" /etc/nginx/sites-available/mail
 sudo ln -s /etc/nginx/sites-available/mail /etc/nginx/sites-enabled/
 sudo nginx -s reload
 
@@ -59,9 +62,9 @@ sudo nginx -s reload
 #https://certbot.eff.org/instructions?ws=nginx&os=snap
 
 while true; do
-    read -p "Do you want to install certboot?(for https) [Y/n] " choice
-    choice=${choice:-Y}  # If the user does not enter anything, it is considerred to be Y
-    case $choice in
+    read -p "Do you want to install certboot?(for https) [Y/n] " CHOICE
+    CHOICE=${CHOICE:-Y}  # If the user does not enter anything, it is considerred to be Y
+    case $CHOICE in
         [Yy]* ) 
             echo "You chose to continue."
             sudo snap install --classic certbot
@@ -79,4 +82,4 @@ while true; do
     esac
 done
 
-echo "Your site is available at this address $domain"
+echo "Your site is available at this address $DOMAIN"
